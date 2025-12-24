f9b15b7ce6cca593066deb9afc7d5cfb17c8420c
Serhii Chumak <s4umak@gmail.com>
2025-12-24 16:38:13 +0200

    Add user

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -12,17 +12,22 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+import environ
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+
+SECRET_KEY = env("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xu9g3tteufg!+1wb4nrqikwt+p-)=+iz@@8ob(o1g7s)808&bg'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,7 +84,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "theatre_service.wsgi.application"
-
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
